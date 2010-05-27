63c2cd577fe9d5bd4f5e15116eb072b8cd730c2b
Massimo Lauria <lauria.massimo@gmail.com>
2010-05-27 21:36:57 +0200

    Fonts dir added.

---
+++ fonts/update-fontdir.sh
@@ -0,0 +1,14 @@
+#!/bin/shD
+
+if [ ! -f $PWD/update-fontdir.sh ]; then
+    echo "Please run the script in its own directory"
+    exit 1
+fi
+
+echo "Update fonts.dir..."
+mkfontdir .
+echo "Update fonts.scale..."
+mkfontscale .
+echo "Update fc.cache-1"
+fc-cache .
+echo "Bye bye"
