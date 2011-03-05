3a030676b52030c82ba45404219093dbbe29349d
Massimo Lauria <lauria.massimo@gmail.com>
2011-03-05 15:17:14 +0100

    Preliminary install script

---
+++ install.sh
@@ -0,0 +1,133 @@
+#!/bin/sh
+#
+# Copyright (C) 2010, 2011 by Massimo Lauria <lauria.massimo@gmail.com>
+#
+# Created   : "2011-03-05, sabato 01:03 (CET) Massimo Lauria"
+# Time-stamp: "2011-03-05, sabato 15:16 (CET) Massimo Lauria"
+
+# Description::
+#
+# My configuration files are rich and require some software to be
+# present.  In some cases the configuration would gracefully ignore
+# the missing software, or it is useless and imappropriate to
+# force the installation of a software.
+#
+# This file will try to setup the configuration in a usable status.
+
+
+# -------------------- Env Variables ------------------------
+GIT=git
+CUT=cut
+CP=cp
+LN=ln
+FILE_NOT_FOUND=127
+
+
+# ------------------- Utilities -----------------------------
+require_program()
+{
+    if [ $# -ne 1 ]; then echo "Wrong argument number."; exit 1; fi
+
+    echo -n "Checking for '$1'... "
+    which $1 2> /dev/null > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "FAIL."
+        exit_on_missing_program $1
+    else
+        echo "OK."
+    fi
+}
+
+exit_on_missing_program() {
+    echo ""
+    echo "Unfortunately program \"$1\" is not present, or it is not executable."
+    echo "Without this software, I can't finish the installation."
+    echo ""
+    echo "Bye bye."
+    exit $FILE_NOT_FOUND
+}
+
+backup_maybe() {
+# Check backup possibility.
+    if [ $# -ne 1 ]; then echo "Wrong argument number."; exit 1; fi
+    if [ -e $1 ]; then
+        cp -arb $1 $1.bak.`date +%Y-%m-%d.%H.%M.%S`
+    fi
+}
+
+issue_warning_on_pwd() {
+    if [ $# -ne 1 ]; then echo "Wrong argument number."; exit 1; fi
+    if [ "$PWD" != "$HOME/$1" ]; then
+        echo ""
+        echo "WARNING: you are installing in a wrong path."
+        echo "WARNING: the right path should be ~/$1."
+        echo "WARNING: installation would go on, but something may not work properly."
+        echo "WARNING: remember that moving folder requires running this script again."
+        echo ""
+    fi
+}
+
+# ------------------- Installation -------------------------
+
+# Goto config folder.
+cd $(dirname $0)
+
+issue_warning_on_pwd "config"
+
+echo "# 1. Check for the present of basic programs"
+require_program $GIT
+require_program $CUT
+require_program $CP
+require_program $LN
+echo ""
+echo ""
+
+
+
+echo "# 2. Check out all submodules"
+#echo "# 2. Determines read/write privileges on the repo."
+$GIT submodule init
+$GIT submodule update
+echo ""
+echo ""
+
+SUBMODULES=`$GIT submodule|$CUT -d' ' -f3`
+
+echo "# 3. Put all submodules to master branch"
+for SUBM in $SUBMODULES; do
+    if [ ! -d "$SUBM" ]; then
+        echo "# 3. Oops! Something in wrong with the submodules list."
+        echo "# 3. Please do not run the install script in a non clean installation."
+        echo ""
+    else
+        echo "# 3. checking out $SUBM master branch"
+        cd $SUBM
+        $GIT co master
+        cd ..
+        echo "# 3. $SUBM master branch cheched out"
+        echo ""
+    fi
+done
+echo ""
+
+echo "# 4. Recursively setup all submodules"
+for SUBM in $SUBMODULES; do
+    if [ ! -d "$SUBM" ]; then
+        echo "# 4. oops! something wrong in the submodules list."
+        echo "# 4. do not run install script in a non clean installation."
+    else
+        if [ -x $SUBM/install.sh ]; then
+            echo "# 4. $SUBM module requires an install procedure"
+            $SUBM/install.sh
+            echo "# 4. $SUBM module installed"
+        fi
+    fi
+done
+echo ""
+echo ""
+
+
+
+# Local Variables:
+# fill-column: 80
+# End:
