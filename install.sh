0cdf4afe7bbb04cec6cb5d695a4641a36666845c
Massimo Lauria <lauria.massimo@gmail.com>
2012-01-30 00:27:01 +0100

    Make install scripts compatible with BSD and Mac cp command

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-# Copyright (C) 2010, 2011 by Massimo Lauria <lauria.massimo@gmail.com>
+# Copyright (C) 2010, 2011, 2012 by Massimo Lauria <lauria.massimo@gmail.com>
 #
 # Created   : "2011-03-05, sabato 01:03 (CET) Massimo Lauria"
-# Time-stamp: "2011-11-06, Sunday 12:12 (CET) Massimo Lauria"
+# Time-stamp: "2012-01-30, 00:17 (CET) Massimo Lauria"
 
 # Description::
 #
@@ -53,7 +53,7 @@ backup_maybe() {
 # Check backup possibility.
     if [ $# -ne 1 ]; then echo "Wrong argument number."; exit 1; fi
     if [ -e $1 ]; then
-        cp -ar $1 $1.bak.`date +%Y-%m-%d.%H.%M.%S`
+        $CP -af $1 $1.bak.`date +%Y-%m-%d.%H.%M.%S`
     fi
 }
 
