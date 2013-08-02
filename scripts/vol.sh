be638f70af8f620c5fa80dcd8f32b679066bcc95
Massimo Lauria <lauria.massimo@gmail.com>
2013-08-03 01:18:09 +0200

    EMacs conf updated

--- scripts/vol.sh
+++ scripts/vol.sh
@@ -22,7 +22,7 @@ NOTIFICATION_ID_FILE_NAME=~/.id_volume_notification_awesome
 command=
 increment=5%
 mute_mixer=Master
- vol_mixer=Master # it is PCM in my home machine
+ vol_mixer=PCM # it is PCM in my home machine
 hdmi_mixer=IEC958
 
 # Determine if I should use 'Master' or 'PCM' to control volume
@@ -61,13 +61,13 @@ icon_name=""
 text_string=" "
 
 if [ "$command" = "up" ]; then
-    # pacmd set-sink-mute 0 0 >/dev/null
+    pacmd set-sink-mute 0 0 >/dev/null
     display_volume=$(amixer set $vol_mixer $increment+ unmute | grep -m 1 "%]" | cut -d "[" -f2|cut -d "%" -f1)
     output_string=`printf "$text_vol__change" "$display_volume"`
 fi
 
 if [ "$command" = "down" ]; then
-    # pacmd set-sink-mute 0 0 >/dev/null
+    pacmd set-sink-mute 0 0 >/dev/null
     display_volume=$(amixer set $vol_mixer $increment- unmute | grep -m 1 "%]" | cut -d "[" -f2|cut -d "%" -f1)
     output_string=`printf "$text_vol__change" "$display_volume"`
 fi
@@ -76,12 +76,12 @@ fi
 if [ "$command" = "mute" ]; then
     if amixer get $mute_mixer | grep "\[on\]"; then
         display_volume=0
-        # pacmd set-sink-mute 0 1 >/dev/null
-        amixer set $mute_mixer mute
+        pacmd set-sink-mute 0 1 >/dev/null
+        # amixer set $mute_mixer mute
         output_string=$text_snd___muted
     else
-        # pacmd set-sink-mute 0 0 >/dev/null
-        amixer set $mute_mixer unmute
+        pacmd set-sink-mute 0 0 >/dev/null
+        # amixer set $mute_mixer unmute
         display_volume=$(amixer get $vol_mixer | grep -m 1 "%]" | cut -d "[" -f2|cut -d "%" -f1)
         output_string=$text_snd_unmuted
     fi
@@ -131,4 +131,3 @@ awesome-client > $NOTIFICATION_ID_FILE_NAME <<EOF
 notification=naughty.notify({ text="$output_string", timeout=1.5, replaces_id=$LAST_ID , icon="$icon_name"});
 return notification.id
 EOF
-
