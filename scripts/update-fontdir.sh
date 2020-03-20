5211523ae036cec568235a804fb285bdf19fd00d
Massimo Lauria <lauria.massimo@gmail.com>
2020-03-20 17:36:49 +0100

    Removed some obsolete files

--- scripts/update-fontdir.sh
+++ scripts/update-fontdir.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#
+# Copy this script in the ~/.fonts folder under Linux, and run it
+# every time you move fonts there in order to make them visible to the
+# font system.
+#
+
+
 if [ ! -f $PWD/update-fontdir.sh ]; then
     echo "Please run the script in its own directory"
     exit 1
