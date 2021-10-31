39e9a73e1eb228a6d275b4f344866e77be848227
Massimo Lauria <massimo.lauria@uniroma1.it>
2021-10-31 16:09:49 +0100

    Script to build vice

---
+++ scripts/build-vice.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Copyright (C) 2021 by Massimo Lauria
+#
+# Created   : "2021-10-31, domenica 15:57 (CET) Massimo Lauria"
+# Time-stamp: "2021-10-31, 16:09 (CET) Massimo Lauria"
+#
+# Description::
+#
+# Build script for VICE
+#
+
+# Make shell more robust
+# (see http://redsymbol.net/articles/unofficial-bash-strict-mode/#sourcing-nonconforming-document)
+set -euo pipefail
+IFS=$'\n\t'
+
+# Code::
+VERSION=vice-3.5
+PREFIX=${HOME}/.local/
+SRCDIR=${PREFIX}/src
+BUILDOPTS="--enable-arch=native --with-resid --enable-native-gtk3ui --enable-ethernet --with-pulse --with-alsa --enable-vte --with-fast-sid --with-jpeg --enable-static-ffmpeg --enable-desktop-files --enable-x64"
+
+
+# Download
+if [ ! -s "${SRCDIR}/${VERSION}" ]; then
+    cd ~/queue/
+    wget -c https://sourceforge.net/projects/vice-emu/files/releases/${VERSION}.tar.gz/download
+    cd "${SRCDIR}"
+    tar vzfx "~/queue/${VERSION}.tar.gz"
+fi
+
+cd "${SRCDIR}/${VERSION}"
+./configure "${BUILDOPTS}" --prefix="${PREFIX}"
+make
+make install
+
+# Local Variables:
+# fill-column: 80
+# End:
